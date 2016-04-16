61de362795e19a0a18452c9ee75a5c7a10d454e4
Massimo Bianchi <bianchi.massimo@gmail.com>
2016-04-16 16:43:56 +0200

    add states constants and state field in ticket model

--- djBug/ticket/models.py
+++ djBug/ticket/models.py
@@ -15,5 +15,19 @@ class Ticket(models.Model):
     A ticket object is a bug notification/improve request. When user sees something
     that he doesn't like in a project, he *open* a new ticket.
     """
+    STATE_TO_APPROVE = "to_approve"
+    STATE_APPROVED = "approved"
+    STATE_CLOSED = "closed"
+    STATE_NOTABUG = "not_a_bug"
+    STATE_WONTFIX = "wontfix"
+    STATES = (
+        (STATE_TO_APPROVE, "To Approve"),
+        (STATE_APPROVED, "Approved"),
+        (STATE_CLOSED, "Closed"),
+        (STATE_NOTABUG, "Not a Bug"),
+        (STATE_WONTFIX, "Won't fix"),
+    )
+
     subject = models.CharField(max_length=40, null=False, blank=False, default=None)
     description = models.TextField()
+    state = models.CharField(max_length=10, choices=STATES, default=STATE_TO_APPROVE)

9c40ef05345e1e9fec6d1cbb4fae340ca6eda78c
Massimo Bianchi <bianchi.massimo@gmail.com>
2016-04-16 16:04:02 +0200

    Initial Import: Created basic django project structure ad ticket app structure.

---
+++ djBug/ticket/tests/test_models.py
@@ -0,0 +1,23 @@
+'''
+DjBug Ticket Models test unit
+===============================
+
+Part of djBug project.
+
+Created on 16 apr 2016
+
+@author: massimo bianchi
+'''
+from django.test.testcases import TestCase
+
+
+class TicketTestCase(TestCase):
+    """
+    Test Case for Ticket Model
+    """
+
+    def test_create(self):
+        """
+        Test creation of a new ticket
+        """
+        self.fail("Incomplete Test")

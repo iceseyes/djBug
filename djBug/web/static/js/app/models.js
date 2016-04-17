cbcf8e8ad87fefc482e3e30f448f2e3b23852ae4
Massimo Bianchi <bianchi.massimo@gmail.com>
2016-04-17 17:29:18 +0200

    * display different icon for different state

--- djBug/web/static/js/app/models.js
+++ djBug/web/static/js/app/models.js
@@ -3,6 +3,17 @@
 var app = app || {};
 
 app.TicketModel = Backbone.Model.extend({
+	initialize: function() {
+		if(this.get("state")==="to_approve") {
+			this.set("state_icon", "folder open");
+		} else if(this.get("state")==="closed") {
+			this.set("state_icon", "close");
+		} else if(this.get("state")==="not_a_bug") {
+			this.set("state_icon", "Thumbs Down");
+		} else {
+			this.set("state_icon", "bug");
+		}
+	}
 });
 
 app.TicketCollection = Backbone.Collection.extend({
